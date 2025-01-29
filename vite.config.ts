25a7116eb63b475883dd23d4712ce7653970341d
ZHU Hao <hao_kr@163.com>
2025-01-29 13:25:58 +0800

    tauri 还原到默认端口

--- vite.config.ts
+++ vite.config.ts
@@ -29,14 +29,14 @@ export default defineConfig(async () => {
     clearScreen: false,
     // 2. tauri expects a fixed port, fail if that port is not available
     server: {
-      port: 49500,
+      port: 1420,
       strictPort: true,
       host: host || false,
       hmr: host
         ? {
           protocol: "ws",
           host,
-          port: 49501,
+          port: 1421,
         }
         : undefined,
       watch: {
